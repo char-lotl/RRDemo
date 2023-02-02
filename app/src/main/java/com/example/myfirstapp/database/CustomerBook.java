4b9e8b75f1028440ddfae5e4619e759aabbcd169
Charlotte Lastname <lancharlotl@gmail.com>
2023-02-02 12:56:15 -0500

    Fixed the CustomerBook schema issue -- book selections will now save properly.

--- app/src/main/java/com/example/myfirstapp/database/CustomerBook.java
+++ app/src/main/java/com/example/myfirstapp/database/CustomerBook.java
@@ -7,6 +7,9 @@ import androidx.room.PrimaryKey;
 @Entity
 public class CustomerBook {
     @PrimaryKey
+    public String entry_id;
+
+    @ColumnInfo(name = "user")
     public int uid;
 
     @ColumnInfo(name = "isbn")
@@ -16,12 +19,14 @@ public class CustomerBook {
     public String student_id;
 
     public CustomerBook(Customer c, String i, String s) {
+        entry_id = c.uid + i;
         uid = c.uid;
         isbn = i;
         student_id = s;
     }
 
     public CustomerBook(int uid, String isbn, String student_id) {
+        this.entry_id = uid + isbn;
         this.uid = uid;
         this.isbn = isbn;
         this.student_id = student_id;
